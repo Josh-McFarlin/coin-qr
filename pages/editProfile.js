087ca6c277979f52764e579248fcdc5adbe25a2a
Josh-McFarlin <jmcfarlin6@gatech.edu>
2020-01-20 02:48:27 -0500

    Converted last page to static

--- pages/editProfile.js
+++ pages/editProfile.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { withRouter } from 'next/router';
+import { useRouter } from 'next/router';
 import _ from 'lodash';
 import {
     Card, CardHeader, CardBody, CardFooter, Row, Col,
     Form, FormGroup, FormInput, FormTextarea, Button
 } from 'shards-react';
-
 import Error from './_error';
 import AddressListEditor from '../src/components/AddressList/AddressListEditor';
 import noProfilePic from '../public/static/images/noProfilePic.png';
-import { updateProfile } from '../src/firebase/actions';
+import { getProfile, updateProfile } from '../src/firebase/actions';
 import urls from '../utils/urls';
+import firebase from '../src/firebase';
 
 
 const styles = () => ({
@@ -44,179 +43,167 @@ const styles = () => ({
 
 const classes = {};
 
-class EditProfilePage extends React.PureComponent {
-    static async getInitialProps({ query, res }) {
-        const { error, profile } = _.get(res, 'locals', {});
+const EditProfilePage = () => {
+    const router = useRouter();
+    const [userId, setUserId] = React.useState(null);
+    const [profile, setProfile] = React.useState(null);
+    const [data, setData] = React.useState({
+        title: '',
+        caption: '',
+        addresses: []
+    });
+    const [error, setError] = React.useState(null);
 
-        return {
-            error,
-            profile
-        };
-    }
+    React.useEffect(() => {
+        firebase.auth()
+            .onAuthStateChanged((user) => {
+                if (user) {
+                    setUserId(user.uid);
 
-    constructor(props) {
-        super(props);
+                    getProfile(user.uid)
+                        .then((prof) => setProfile(prof))
+                        .catch(() => setError({
+                            statusCode: 404,
+                            message: 'Unable to get profile!'
+                        }));
+                } else {
+                    setUserId(null);
+                }
+            }, () => {
+                setUserId(null);
+            });
+    }, []);
 
-        const profile = _.get(props, 'profile', null);
-        const data = _.get(profile, 'data', {});
-
-        this.state = {
-            data,
-            editError: null
-        };
-    }
-
-    handleChange = ({ target: { id, value } }) => {
-        const { data } = this.state;
+    const handleChange = ({ target: { id, value } }) => {
         const dataCopy = _.cloneDeep(data);
 
-        this.setState({
-            data: _.set(dataCopy, id, value)
+        setData(_.set(dataCopy, id, value));
+    };
+
+    const updateAddresses = (addresses) => {
+        if (_.get(error, 'type') === 'addresses' && addresses.length > 0) {
+            setError(null);
+        }
+
+        setData((prevData) => {
+            const dataCopy = { ...prevData };
+
+            return _.set(dataCopy, 'addresses', addresses);
         });
     };
 
-    updateAddresses = (addresses) => {
-        const { data } = this.state;
-        const dataCopy = _.cloneDeep(data);
-
-        this.setState((prevState) => ({
-            data: _.set(dataCopy, 'addresses', addresses),
-            editError: _.get(prevState, 'editError.type') === 'addresses' && addresses.length > 0 ? null : prevState.editError
-        }));
-    };
-
-    submitJson = () => {
-        const { router, profile } = this.props;
-        const { data } = this.state;
-
+    const submitJson = async () => {
         if (data.addresses.length > 10) {
-            this.setState({
-                editError: {
-                    type: 'addresses',
-                    message: `Error: A profile can contain a maximum of 10 addresses, please remove at least ${data.addresses.length - 10} addresses!`
-                }
+            setError({
+                type: 'addresses',
+                message: `Error: A profile can contain a maximum of 10 addresses, please remove at least ${data.addresses.length - 10} addresses!`
             });
         } else if (!_.isEqual(data, profile.data)) {
-            updateProfile(data, profile.profileId)
-                .then(() => router.push(urls.profile.view(profile.profileId)));
+            await updateProfile(userId, data)
+                .then(() => router.push(urls.profile.view(userId)));
         } else {
-            router.push(urls.profile.view(profile.profileId));
+            await router.push(urls.profile.view(userId));
         }
     };
 
-    render() {
-        const { error } = this.props;
-        const { data, editError } = this.state;
-
-        if (_.isObject(error)) {
-            return (
-                <Error
-                    statusCode={error.statusCode}
-                    statusMessage={error.message}
-                />
-            );
-        }
-
-        const pictureSrc =
-            _.has(data, 'picture') && !_.isEmpty(data.picture) ?
-                data.picture : noProfilePic;
-
-        const profileSection = (
-            <Card>
-                <CardHeader className={classes.header}>
-                    Profile
-                </CardHeader>
-                <CardBody>
-                    <Row className={classes.rowNoPadding}>
-                        <Col sm={2}>
-                            <Row className={classes.fullHeight}>
-                                <Col className={classes.profileCol}>
-                                    <img
-                                        className={classes.profilePicture}
-                                        src={pictureSrc}
-                                        alt='Profile'
-                                    />
-                                </Col>
-                            </Row>
-                        </Col>
-                        <Col sm={10}>
-                            <Form>
-                                <FormGroup>
-                                    <label htmlFor='name'>Name</label>
-                                    <FormInput
-                                        id='name'
-                                        onChange={this.handleChange}
-                                        value={_.get(data, 'name', '')}
-                                    />
-                                </FormGroup>
-                                <FormGroup>
-                                    <label htmlFor='picture'>Profile Picture URL</label>
-                                    <FormInput
-                                        id='picture'
-                                        onChange={this.handleChange}
-                                        value={_.get(data, 'picture', '')}
-                                    />
-                                </FormGroup>
-                                <FormGroup>
-                                    <label htmlFor='bio'>Bio</label>
-                                    <FormTextarea
-                                        id='bio'
-                                        onChange={this.handleChange}
-                                        value={_.get(data, 'bio', '')}
-                                    />
-                                </FormGroup>
-                            </Form>
-                        </Col>
-                    </Row>
-                </CardBody>
-                <CardFooter>
-                    <Button
-                        theme='success'
-                        onClick={this.submitJson}
-                        className={classes.floatRight}
-                    >
-                        Save Page
-                    </Button>
-                </CardFooter>
-            </Card>
-        );
-
-        const addressSection = (
-            <AddressListEditor
-                className={classes.fullHeight}
-                addresses={_.get(data, 'addresses', [])}
-                error={editError}
-                updateAddresses={this.updateAddresses}
+    if (_.isObject(error)) {
+        return (
+            <Error
+                statusCode={error.statusCode}
+                statusMessage={error.message}
             />
         );
-
-        return (
-            <Row>
-                <Col>
-                    <Row>
-                        <Col>
-                            {profileSection}
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            {addressSection}
-                        </Col>
-                    </Row>
-                </Col>
-            </Row>
-        );
     }
-}
 
-EditProfilePage.propTypes = {
-    profile: PropTypes.object.isRequired,
-    router: PropTypes.object.isRequired,
-    error: PropTypes.object
-};
+    const pictureSrc =
+        _.has(data, 'picture') && !_.isEmpty(data.picture) ?
+            data.picture : noProfilePic;
 
-EditProfilePage.defaultProps = {
-    error: undefined
+    const profileSection = (
+        <Card>
+            <CardHeader className={classes.header}>
+                Profile
+            </CardHeader>
+            <CardBody>
+                <Row className={classes.rowNoPadding}>
+                    <Col sm={2}>
+                        <Row className={classes.fullHeight}>
+                            <Col className={classes.profileCol}>
+                                <img
+                                    className={classes.profilePicture}
+                                    src={pictureSrc}
+                                    alt='Profile'
+                                />
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col sm={10}>
+                        <Form>
+                            <FormGroup>
+                                <label htmlFor='name'>Name</label>
+                                <FormInput
+                                    id='name'
+                                    onChange={handleChange}
+                                    value={_.get(data, 'name', '')}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <label htmlFor='picture'>Profile Picture URL</label>
+                                <FormInput
+                                    id='picture'
+                                    onChange={handleChange}
+                                    value={_.get(data, 'picture', '')}
+                                />
+                            </FormGroup>
+                            <FormGroup>
+                                <label htmlFor='bio'>Bio</label>
+                                <FormTextarea
+                                    id='bio'
+                                    onChange={handleChange}
+                                    value={_.get(data, 'bio', '')}
+                                />
+                            </FormGroup>
+                        </Form>
+                    </Col>
+                </Row>
+            </CardBody>
+            <CardFooter>
+                <Button
+                    theme='success'
+                    onClick={submitJson}
+                    className={classes.floatRight}
+                >
+                    Save Page
+                </Button>
+            </CardFooter>
+        </Card>
+    );
+
+    const addressSection = (
+        <AddressListEditor
+            className={classes.fullHeight}
+            addresses={_.get(data, 'addresses', [])}
+            error={error}
+            updateAddresses={updateAddresses}
+        />
+    );
+
+    return (
+        <Row>
+            <Col>
+                <Row>
+                    <Col>
+                        {profileSection}
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        {addressSection}
+                    </Col>
+                </Row>
+            </Col>
+        </Row>
+    );
 };
 
 export default EditProfilePage;
